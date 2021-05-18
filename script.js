48f602186d56b614f6a53e6f45d955652161ab92
youcef benabderrezak <youcef12091997@gmail.com>
2021-05-18 14:38:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+new Vue({
+    el: '#app', //el : elemnt
+    data: {
+        counte: 0
+    },
+
+    methods: {
+
+        increment: function() {
+            this.counte++
+        },
+        decrement: function() {
+            this.counte--
+        },
+        addStep: function(step) {
+            this.counte += step
+        },
+        test: function() {
+            alert('test alert')
+        },
+        logName: function() {
+            console.log('test input log')
+        }
+
+    },
+
+    computed: {
+        calcul: function() {
+            return this.counte * 7
+        }
+
+    },
+    watch: {
+
+    }
+})
